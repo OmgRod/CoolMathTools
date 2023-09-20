2d93a3f0258443b6e5dd80f225d0585d768e11cb
0mgRod <rodrigofonseca20@outlook.com>
2023-09-20 21:23:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+    res.render('index', { pageTitle: 'Home' });
+});
+
+// Define a route for tools
+app.get('/tools/calculator', (req, res) => {
+    res.render('tools/calculator', { pageTitle: 'Calculator Tool' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
